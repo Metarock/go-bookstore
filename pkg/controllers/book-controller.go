d2b9d49d6bf198e0e1b2357bf62a8b09f200b3ee
John Isaiah Sangalang <esang037@gmail.com>
2023-08-18 16:18:57 +1200

    start controller

---
+++ pkg/controllers/book-controller.go
@@ -0,0 +1,76 @@
+package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+
+	"github.com/Metarock/go-bookstore/pkg/models"
+	"github.com/Metarock/go-bookstore/pkg/utils"
+)
+
+var NewBook models.Book
+
+func GetBook(writer http.ResponseWriter, request *http.Request) {
+	newBooks := models.GetAllBooks()
+
+	res, _ := json.Marshal(newBooks)
+
+	writer.Header().Set("Content-Type", "pkglication/json")
+	writer.WriteHeader(http.StatusOK)
+	writer.Write(res)
+}
+
+func GetBookById(writer http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+
+	bookId := vars["bookId"]
+
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Println("Error while parsing")
+	}
+
+	bookDetails, _ := models.GetBookById(ID)
+
+	res, _ := json.Marshal(bookDetails)
+
+	writer.Header().Set("Content-Type", "pkglication/json")
+	writer.WriteHeader(http.StatusOK)
+	writer.Write(res)
+}
+
+func CreateBook(writer http.ResponseWriter, request *http.Request) {
+	CreateBook := &models.Book{}
+
+	utils.ParseBody(request, CreateBook)
+
+	book := CreateBook.CreateBook()
+
+	res, _ := json.Marshal(book)
+
+	writer.WriteHeader(http.StatusOK)
+	writer.Write(res)
+}
+
+func DeleteBook(writer http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+
+	bookId := vars["bookId"]
+
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Println("Error while parsing")
+	}
+
+	book := models.DeleteBook(ID)
+
+	res, _ := json.Marshal(book)
+
+	writer.Header().Set("content-type", "pkglication/json")
+	writer.WriteHeader(http.StatusOK)
+	writer.Write(res)
+}
