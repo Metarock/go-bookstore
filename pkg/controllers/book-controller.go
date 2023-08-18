bec979bae50e81b5f91733cb98b41a180ab104a6
John Isaiah Sangalang <esang037@gmail.com>
2023-08-18 16:30:17 +1200

    completed controller

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -74,3 +74,42 @@ func DeleteBook(writer http.ResponseWriter, request *http.Request) {
 	writer.WriteHeader(http.StatusOK)
 	writer.Write(res)
 }
+
+func UpdateBook(writer http.ResponseWriter, request *http.Request) {
+	var updatedBook = &models.Book{}
+
+	utils.ParseBody(request, updatedBook)
+
+	vars := mux.Vars(request)
+
+	bookId := vars["bookId"]
+
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+
+	if err != nil {
+		fmt.Println("Error while parsing")
+	}
+
+	// update the book by finding it
+
+	bookDetails, db := models.GetBookById(ID)
+	if updatedBook.Name != "" {
+		bookDetails.Name = updatedBook.Name
+	}
+
+	if updatedBook.Author != "" {
+		bookDetails.Author = updatedBook.Author
+	}
+
+	if updatedBook.Publication != "" {
+		bookDetails.Publication = updatedBook.Publication
+	}
+
+	db.Save(&bookDetails)
+
+	res, _ := json.Marshal(bookDetails)
+
+	writer.Header().Set("Content-Type", "pkglication/json")
+	writer.WriteHeader(http.StatusOK)
+	writer.Write(res)
+}
